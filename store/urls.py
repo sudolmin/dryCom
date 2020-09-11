ed7da2633195372d3ff4d07e8959458983911c77
sudolmin <61926180+sudolmin@users.noreply.github.com>
2020-09-11 19:14:29 +0530

    n

--- store/urls.py
+++ store/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from . import api
+from . import api, views
 
 urlpatterns = [
+	path('', views.Store, name="Store"),
+	path('cart/', views.Cart, name="Cart"),
+	path('checkout/', views.CheckOut, name="CheckOut"),
 	# api urlpatterns
 	path('category-api/', api.CateView, name="cate_api_view"),
 ]
--- store/views.py
+++ store/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def Store(request):
+	return render(request, 'home.html')
+
+def Cart(request):
+	return render(request, 'cart.html')
+
+def CheckOut(request):
+	return render(request, 'checkout.html')
